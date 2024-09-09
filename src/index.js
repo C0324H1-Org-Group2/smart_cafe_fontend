56429e24a2fc393976666e8502cedbdc90c4b24d
daohuyhoang <71455374+daohuyhoang@users.noreply.github.com>
2024-09-09 16:19:23 +0700

    kick tinh

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Admin from "./admin";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App/>
-        <Admin/>
     </React.StrictMode>
 );
 
