42786c326a149440a8a25988ec3b440d37a92776
trantheanh3101 <trantheanhk11@gmail.com>
2024-09-11 08:10:34 +0700

    update BillDetail

--- src/components/client/services/ServiceService.js
+++ src/components/client/services/ServiceService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_BASE_URL = 'http://localhost:8080/api/client';
+
 export const getMenuItems = async ()=> {
     try {
         const response = await axios.get('http://localhost:8080/api/services/list-service-types');
@@ -29,16 +31,6 @@ export const getTop5MostOrderedServices = async () => {
     }
 }
 
-// export const getAllServices = async () => {
-//     try {
-//         const response = await axios.get('http://localhost:8080/api/services/all-services');
-//         return response.data;
-//     } catch (e) {
-//         console.error('Lỗi lấy tất cả :', e);
-//         return [];
-//     }
-// }
-
 export const getServicesByType = async (typeId) => {
     try {
         const response = await axios.get(`http://localhost:8080/api/services/services-by-type/${typeId}`);
@@ -48,3 +40,37 @@ export const getServicesByType = async (typeId) => {
         return [];
     }
 };
+
+// Lấy bảng ngẫu nhiên có trạng thái isOn = true
+export const getRandomAvailableTable = async () => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/table/random`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching random available table:', error);
+        throw error;
+    }
+};
+
+// Gọi API để cập nhật trạng thái bảng và tạo hóa đơn
+export const updateTableStatusAndCreateBill = async (tableId) => {
+    try {
+        const response = await axios.patch(`${API_BASE_URL}/tables/${tableId}/status`);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating table status and creating bill:', error);
+        throw error;
+    }
+};
+
+// Gửi yêu cầu gọi món
+export const orderItems = async (items) => {
+    try {
+        await axios.post(`${API_BASE_URL}/bill-details/order`, items);
+    } catch (error) {
+        console.error('Error ordering items:', error);
+        throw error;
+    }
+};
+
+
